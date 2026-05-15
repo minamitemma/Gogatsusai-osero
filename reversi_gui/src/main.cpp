619c75661618505014faa5ab03e9eb98a6599b73
Sena Goto <a81906@MacBook-Pro-70.local>
2026-05-16 08:32:59 +0900

    add reset names

--- reversi_gui/src/main.cpp
+++ reversi_gui/src/main.cpp
@@ -1,9 +1,54 @@
 #include <QApplication>
+#include <QCommandLineParser>
+#include <QDir>
+#include <QFile>
+#include <QStandardPaths>
+#include <QTextStream>
+
 #include "mainwindow.h"
+
+namespace
+{
+
+QString rankingFilePath()
+{
+    QString basePath = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    if (basePath.isEmpty()) {
+        basePath = QDir::homePath() + "/.reversi_gui";
+    }
+    return QDir(basePath).filePath("rankings.json");
+}
+
+void resetRankingFile()
+{
+    const QString path = rankingFilePath();
+    if (QFile::exists(path)) {
+        QFile::remove(path);
+    }
+}
+
+} // namespace
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QCoreApplication::setApplicationName("ReversiGUI");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("6x6 Reversi GUI");
+    parser.addHelpOption();
+
+    QCommandLineOption resetOption(
+        "reset",
+        "Delete saved ranking records before starting."
+    );
+    parser.addOption(resetOption);
+    parser.process(app);
+
+    if (parser.isSet(resetOption)) {
+        resetRankingFile();
+        QTextStream(stdout) << "Ranking records reset: " << rankingFilePath() << Qt::endl;
+    }
     
     MainWindow window;
     window.setWindowTitle("Reversi Game - 6x6");
